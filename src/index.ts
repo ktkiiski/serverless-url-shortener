7af3cd174aaf1693920c1b3ebe55c307eb0596b3
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2017-03-25 22:55:09 +0200

    Removed a test partial template

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
 import './index.scss';
 
 // Write your code here!
-
-const element = document.createElement('div');
-element.innerHTML = require<string>('./example.tmpl');
-document.body.appendChild(element);
