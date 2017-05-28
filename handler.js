1f8cea1541d7f602ed58a00a5b04336c422f5a24
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2017-05-28 22:57:13 +0300

    Improved shortening API

--- handler.js
+++ handler.js
@@ -14,73 +14,52 @@ const s3Bucket = process.env['S3_BUCKET'];
 const s3Region = process.env['S3_REGION'];
 const s3Prefix = 'u/';
 
-
-// generate a 7 char shortid
-
-const shortid = () => {
+function shortId() {
     return 'xxxxxxx'.replace(/x/g, (c) => {
         return (Math.random() * 36 | 0).toString(36);
     });
 }
 
-exports.shortenUrl = (event, context, cb) => {
-    const headers = event.headers;
-    const {url_long, key} = JSON.parse(event.body);
-    const s3 = new AWS.S3({ region: s3Region });
-    let retry = 0;    // try at most 3 times to create unique id
+exports.shortenUrl = (event, context, callback) => {
 
-    const done = (url_short, error) => {
-        cb(null, {
-            statusCode: error ? 400 : 200,
-            body: JSON.stringify({ url_long: url_long, url_short: url_short, error: error }),
-        });
-    };
-
-    const check_and_create_s3_redirect = (s3_bucket, key_short, url_long) => {
-        s3.headObject({ Bucket: s3_bucket, Key: key_short }, (err, data) => {
-            if (err) {
-                // we should normall have a NotFound error showing that the id is not already in use
-                if (err.code === "NotFound") {
-                    // normal execution path
-                    s3.putObject({
-                        Bucket: s3_bucket,
-                        Key: key_short,
-                        Body: "",
-                        WebsiteRedirectLocation: url_long,
-                        ContentType: "text/plain",
-                    },
-                        (err, data) => {
-                            if (err) { done("", err.message); }
-                            else {
-                                const ret_url = `http://${domain}/${id_short}`;
-                                console.log("Success, short_url = " + ret_url);
-                                done(ret_url, "");
-                            }
-                        });
-                } else {
-                    // treat all other errors as fatal
-                    done("", "Could not find an suitable name, error: " + err.code);
-                }
-            } else {
-                // we found a duplicate, let's retry a limited number of times
-                retry += 1;
-                if (retry <= 3) {
-                    check_and_create_s3_redirect(s3_bucket, key_short, url_long);
-                } else {
-                    // abort after 3 tries
-                    done("", "Cannot find an unused short id, aborting.");
-                }
-            }
-        });
+    function respond(statusCode, responseData) {
+        const body = JSON.stringify(responseData);
+        const headers = {'Content-Type': 'application/json'};
+        callback(null, {statusCode, body, headers});
     }
 
+    const headers = event.headers;
+    let {longUrl, key} = JSON.parse(event.body);
+    if (key) {
+        // Remove all non-word characters
+        key = key.replace(/[^a-z0-9_]/ig, '');
+    } else {
+        key = shortId();
+    }
     // check if url is valid
-    const url_check = url.parse(url_long);
-    if (!((url_check) && (url_check.host))) { return done("", "Invalid URL format"); }
+    const urlCheck = url.parse(longUrl);
+    if (!((urlCheck) && (urlCheck.host))) {
+        return respond(400, {detail: "Invalid URL format"});
+    }
+    const s3 = new AWS.S3({ region: s3Region });
 
-    console.log("Long URL to shorten: " + url_long);
-    const id_short = key || shortid();
-    const key_short = `${s3Prefix}${id_short}`;
-    console.log("Short id = " + key_short);
-    check_and_create_s3_redirect(s3Bucket, key_short, url_long);
+    s3.putObject(
+        {
+            Bucket: s3Bucket,
+            Key: `${s3Prefix}${key}`,
+            Body: "",
+            WebsiteRedirectLocation: longUrl,
+            ContentType: "text/plain",
+        },
+        (err, data) => {
+            if (err) {
+                respond(400, {detail: err.message});
+            }
+            else {
+                const shortUrl = `http://${domain}/${key}`;
+                console.log(`Successfully shortened ${longUrl} to ${shortUrl}`);
+                respond(200, {longUrl: longUrl, shortUrl: shortUrl});
+            }
+        }
+    );
 };
