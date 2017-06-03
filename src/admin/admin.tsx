6d0af89802a78de15c69eb89b30f1798f5a7ec18
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2017-06-03 19:23:07 +0300

    Simple URL validation

--- src/admin/admin.tsx
+++ src/admin/admin.tsx
@@ -2,14 +2,19 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './admin.scss';
 
+// tslint:disable:max-line-length
+const URL_REGEXP = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+
 interface IURLShortenerState {
     longUrl: string;
+    isSubmitEnabled: boolean;
 }
 
 class URLShortenerForm extends React.Component<{}, IURLShortenerState> {
 
     public state: IURLShortenerState = {
         longUrl: '',
+        isSubmitEnabled: false,
     };
 
     private fieldId = `urlField_${Math.floor(Math.random() * 10000)}`;
@@ -27,13 +32,22 @@ class URLShortenerForm extends React.Component<{}, IURLShortenerState> {
                         value={this.state.longUrl}
                         onChange={this.handleChange} />
                 </div>
-                <button type='submit' className='btn btn-primary'>Shorten</button>
+                <button
+                    type='submit'
+                    className='btn btn-primary'
+                    disabled={!this.state.isSubmitEnabled}>Shorten</button>
             </form>
         );
     }
 
     public handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-        this.setState({longUrl: event.currentTarget.value});
+        const longUrl = event.currentTarget.value;
+        this.setState((prevState) => {
+            return {
+                ...prevState, longUrl,
+                isSubmitEnabled: !!longUrl && URL_REGEXP.test(longUrl),
+            };
+        });
     }
 
     public handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
