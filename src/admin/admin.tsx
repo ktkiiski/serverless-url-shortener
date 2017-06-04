11b304792ccc58d7fe1bebeb954f8b00a55a438e
Kimmo Kiiski <kimmo.kiiski@gmail.com>
2017-06-04 15:06:35 +0300

    Render URL shortening results

--- src/admin/admin.tsx
+++ src/admin/admin.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 import './admin.scss';
 
 // tslint:disable:max-line-length
 const URL_REGEXP = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
 
-interface IURLShortenerState {
+interface IURLSubmission {
     longUrl: string;
     key: string;
+}
+
+interface IURLResult extends IURLSubmission {
+    shortUrl: string;
+}
+
+interface IURLShortenerState extends IURLSubmission {
     isSubmitEnabled: boolean;
+    results: IURLResult[];
 }
 
 function generateRandomKey(): string {
@@ -20,12 +28,21 @@ class URLShortenerForm extends React.Component<{}, IURLShortenerState> {
 
     private longUrl$ = new BehaviorSubject('');
     private key$ = new BehaviorSubject(generateRandomKey());
-
+    private submission$ = new Subject<IURLSubmission>();
+    private results$ = this.submission$
+        .concatMap((submission) => Observable.timer(1000).mapTo({
+            ...submission,
+            shortUrl: `https://kii.ski/${submission.key}`,
+        } as IURLResult))
+        .scan((results, result) => [...results, result], [])
+    ;
     private state$ = Observable
         .combineLatest(
-            this.longUrl$, this.key$,
-            (longUrl, key) => ({
-                longUrl, key,
+            this.longUrl$,
+            this.key$,
+            this.results$.startWith([]),
+            (longUrl, key, results) => ({
+                longUrl, key, results,
                 isSubmitEnabled: !!longUrl && !!key && URL_REGEXP.test(longUrl),
             } as IURLShortenerState),
         )
@@ -75,6 +92,9 @@ class URLShortenerForm extends React.Component<{}, IURLShortenerState> {
                     type='submit'
                     className='btn btn-primary'
                     disabled={!this.state.isSubmitEnabled}>Shorten</button>
+                {this.state.results.map((result, index) =>
+                    <div key={index}>Shortened {result.longUrl} as {result.shortUrl}</div>,
+                )}
             </form>
         );
     }
@@ -102,7 +122,8 @@ class URLShortenerForm extends React.Component<{}, IURLShortenerState> {
 
     private handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        alert(`Shorten URL ${this.state.longUrl} as https://kii.ski/${this.state.key}`);
+        const {longUrl, key} = this.state;
+        this.submission$.next({longUrl, key});
     }
 }
 
